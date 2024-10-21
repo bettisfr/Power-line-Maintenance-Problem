d97525fa87166e30fcd2a4f516328cb2544ae650
francesco <francesco.betti.sorbelli@gmail.com>
2024-10-21 18:04:59 +0200

    clean

--- code/src/output.cpp
+++
@@ -1,8 +0,0 @@
-#include "output.h"
-
-void save_output(const input &par, vector<solution> &results) {
-    string filename = "output/" + par.exp_name + ".csv";
-    ofstream file(filename, ofstream::out | ofstream::trunc);
-
-    // to implement
-}
